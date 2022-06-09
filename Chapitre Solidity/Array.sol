c0dc4b848728c39f29fb60513a9f6d8829ac1f27
JB <theshitcoinert@gmail.com>
2022-06-09 15:31:24 +0200

    Array avancé

--- Chapitre Solidity/Array.sol
+++ Chapitre Solidity/Array.sol
@@ -3,12 +3,20 @@
 pragma solidity 0.8.14;
 
 contract Whitelist {
-
-    struct Person {  //structure de données
+    struct Person {
+        //structure de données
         string name;
         uint256 age;
     }
 
     Person[] public persons; //tableau public de type porsonne
 
+    function add(string memory _name, uint256 _age) public {
+        Person memory person = Person(_name, _age);
+        persons.push(person);
+    }
+
+    function remove() public {
+        persons.pop();
+    }
 }
