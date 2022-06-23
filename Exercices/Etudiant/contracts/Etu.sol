d78de9743b7e4d012a301274615c97b6f934761e
JB <theshitcoinert@gmail.com>
2022-06-23 23:54:28 +0200

    Etudiants

---
+++ Exercices/Etudiant/contracts/Etu.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Etu {
+  struct Etudiant {
+    string name;
+    uint note;
+  }
+
+  mapping (address=>Etudiant) public EtudiantsMap;
+  Etudiant[] public EtudiantsArray;
+
+  enum classe {
+    sixieme,
+    cinquieme,
+    quatrieme
+  }
+
+  classe public Classe;
+
+    function setetudiant(address _addr, string memory _name, uint _note) public {
+        EtudiantsMap[_addr]=Etudiant(_name, _note);
+    }
+
+    function deletetudiant(address _addr) public {
+        EtudiantsMap[_addr]=Etudiant('', 0);
+    }
+
+    function setenum (uint _classe) public {
+      Classe=classe(_classe);
+    }
+
+}
--- Exercices/Etudiant/truffle-config.js
+++ Exercices/Etudiant/truffle-config.js
@@ -1,14 +1,16 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
 
+
+
 module.exports = {
   networks: {
-    development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    },
-    ropsten:{
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },  
+     ropsten:{
       provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://ropsten.infura.io/v3/${process.env.INFURA_ID}`})},
       network_id:3,
     },
@@ -17,8 +19,8 @@ module.exports = {
       network_id:42,
     },
   },
-  
-  // Set default mocha options here, use special reporters, etc.
+
+  // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
   },
@@ -27,13 +29,13 @@ module.exports = {
     solc: {
       version: "0.8.13",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
-      optimizer: {
-      enabled: false,
-      runs: 200
-      },
-      evmVersion: "london"
-      }
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+      //  evmVersion: "byzantium"
+       }
     }
   },
 };
