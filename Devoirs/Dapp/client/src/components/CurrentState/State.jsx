0f9129686612e0a4cfce1eb36df3a4ed74f96ce4
JB <theshitcoinert@gmail.com>
2022-07-15 15:14:53 +0200

    CurrentState

--- Devoirs/Dapp/client/src/components/CurrentState/State.jsx
+++ Devoirs/Dapp/client/src/components/CurrentState/State.jsx
@@ -1,7 +1,9 @@
+import useEth from "../../contexts/EthContext/useEth";
 
 
-
-
+function getState(){
+    const { state: { contract, accounts } } = useEth();
+}
 
 // contract.methods.workflowStatus().call();
 // await contract.methods.workflowStatus.call();
