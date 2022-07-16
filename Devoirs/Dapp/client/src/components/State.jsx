f202df69bd1e2404ebec5dff82fe31d84603516a
JB <theshitcoinert@gmail.com>
2022-07-17 00:02:46 +0200

    State conditionnel OK

--- Devoirs/Dapp/client/src/components/State.jsx
+++ Devoirs/Dapp/client/src/components/State.jsx
@@ -6,17 +6,18 @@ import { useState } from "react";
 function GetState(){
     const { state: { contract, accounts } } = useEth();
     const [value, readState] = useState("");
+    const [owner, setOwner] = useState("");
     let workflowStatus = ['Admin is registering voters','Proposals registration started','Proposals registration ended','Voting session started','Voting session ended','Votes tallied'];
 
     useEffect(() => {
         if(contract){
+            setTheOwner();
             theState();
         }          
     });
 
     async function theState(){
       const data =  await contract.methods.workflowStatus().call({from: accounts[0]});
-        console.log(data);
         readState(workflowStatus[data]);
     }
 
@@ -33,16 +34,30 @@ function GetState(){
     };
     
 
+
+async function setTheOwner(){
+    const addr = await contract.methods.owner().call();
+    console.log(owner);
+    setOwner(addr);
+}
+
+if(contract) {
+    if (owner===accounts[0]) {
     return(
         <div><div>
             <button onClick={newState}>ChangeState</button>
         </div>
         <div>
-        {/* <div><button onClick={theState}>Voir le state</button></div> */}
         <p>{value}</p>
         </div></div>
-    )
-    
+    );}
+else{
+    return(
+        <div>
+        <p>{value}</p>
+        </div>
+    );}}
+
 }
 
 export default GetState;
