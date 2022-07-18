d5f4cb81420403aa770a752d814487d906837d4f
JB <theshitcoinert@gmail.com>
2022-07-18 13:43:19 +0200

    MAJ

--- Devoirs/Dapp/client/src/components/Proposals/ProposalsArray.jsx
+++ Devoirs/Dapp/client/src/components/Proposals/ProposalsArray.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 
 function ProposalsArray() {
-    const { state: { contract, accounts} } = useEth();
+    const { state: { contract, accounts } } = useEth();
     const [propoID, setPropId] = useState([]);
     // const [value, readProposal] = useState([]);
 
     useEffect(() => {
         if (contract) {
             tableau()
+            // .then
         }
     });
 
@@ -42,15 +43,21 @@ function ProposalsArray() {
     // console.log(propoID)
     // console.log(propoID)
     // console.log(propoID[0])
-    
+
     return (
         <div>
             <table>
+                <thead>
+                    <tr>
+                        <th>Proposals Id</th>
+                        <th>Proposals Description</th>
+                    </tr>
+                </thead>
                 <tbody>
                     {propoID.map((propo) => (
-                        <tr>
-                            <td>{propo.returnValues.proposalId}</td>                            
-                            <td>{propo.returnValues.description}</td>                            
+                        <tr key={propo.returnValues.proposalId}>
+                            <td>{propo.returnValues.proposalId}</td>
+                            <td>{propo.returnValues.description}</td>
                         </tr>
                     ))}
 
