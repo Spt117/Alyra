37f9c10190607968fab985efe838cc24fbfe693c
JB <theshitcoinert@gmail.com>
2022-07-18 13:32:54 +0200

    MAJ

--- Devoirs/Dapp/client/src/components/Proposals/ProposalsArray.jsx
+++ Devoirs/Dapp/client/src/components/Proposals/ProposalsArray.jsx
@@ -1,73 +1,63 @@
-import React, { Component } from "react";
-import Voting from "/home/jb/Projets/Alyra/Devoirs/Dapp/client/src/contracts/Voting.json";
-import getWeb3 from "../getWeb3";
+import { useEffect, useState } from "react";
+import useEth from "../../contexts/EthContext/useEth";
 
-class ProposalsArray extends Component {
-  state = {  web3: null, accounts: null, contract: null, numero: null };
-  
-  componentDidMount = async () => {
-    
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+function ProposalsArray() {
+    const { state: { contract, accounts} } = useEth();
+    const [propoID, setPropId] = useState([]);
+    // const [value, readProposal] = useState([]);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+    useEffect(() => {
+        if (contract) {
+            tableau()
+        }
+    });
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = Voting.networks[networkId];
-      const instance = new web3.eth.Contract(
-        Voting.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      let options = {
-        fromBlock: 0,                  //Number || "earliest" || "pending" || "latest"
-        toBlock: 'latest'
-      };
-
-      // let arrayProposals =[];
-
-      const listNumber = await instance.getPastEvents('ProposalRegistered', options);
-    console.log(listNumber);
-    
-    
-
-      console.log(listNumber.length);
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance, numero: listNumber});
-      
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+    async function tableau() {
+        let options = {
+            fromBlock: 0,                  //Number || "earliest" || "pending" || "latest"
+            toBlock: 'latest'
+        };
+        const listProposals = await contract.getPastEvents('ProposalRegistered', options);
+        setPropId(listProposals);
+        // getPropo(listProposals.returnValues.proposalId)
+        // console.log(listProposals)
     }
-    
-  };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+
+    // console.log(Object.keys(propoID))
+
+    // if (contract) {
+    // async function getPropo(i) {
+    //     // for (let i = 0; i < propoID.length; i++) {
+    //         const proposal = await contract.methods.getOneProposal(i).call({ from: accounts[0]});
+    //         readProposal(proposal)
+    //     // }
+    // }}
+
+
+    // console.log(getPropo(0));
+
+    // let ArrayPropo = [];
+
+    // console.log(propoID)
+    // console.log(propoID)
+    // console.log(propoID[0])
     
     return (
-      <div>
-        
-        <table>
-        {this.state.numero.map((addresse) => (
-          <tr>
-            <td>{addresse.returnValues.proposalId}</td>
-          <td>{addresse.returnValues.description}</td>
-          </tr>
-        ))}
-        </table>
-      </div>
+        <div>
+            <table>
+                <tbody>
+                    {propoID.map((propo) => (
+                        <tr>
+                            <td>{propo.returnValues.proposalId}</td>                            
+                            <td>{propo.returnValues.description}</td>                            
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+        </div>
     );
-  }
 }
 
 
