3e32b74018cffd31fc6b0c57c9820ba002184841
JB <theshitcoinert@gmail.com>
2022-08-16 11:06:33 +0200

    MAJ

--- Devoirs/Vote.sol
+++ Devoirs/Vote.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.14;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Voting is Ownable {
